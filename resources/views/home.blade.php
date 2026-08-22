8e77f7f2f96e20febf688b9303ddff0ed11d87ae
Jose-76825708 <76825708@continental.edu.pe>
2026-08-22 14:15:14 -0500

    finalización de la pagina principal home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -41,9 +41,12 @@
 
             <div class="flex-1 flex p-8 py-20 px-20 pl-30 flex-col gap-5">
 
-                <h1 class="flex gap-0 text-[#0f3c2b]  text-[4em] font-bold">Encuentra la planta perfecta para tu <br> espacio</h1>
-                <p class="flex text-[1.5em] text-[#787878]">Nuestro sistema analiza las condiciones reales <br> de tu hogar y te recomienda las plantas que <br> mejor se adaptan a ti.</p>
-                <a class="flex items-center px-1 py-5 justify-center text-[1.5em] w-[65%] bg-[#7cb22b] text-white rounded-[20px] font-bold hover:scale-110 transition duration-300" href="#">Empezar ahora</a>
+                <h1 class="flex gap-0 text-[#0f3c2b]  text-[4em] font-bold">Encuentra la planta perfecta para tu <br>
+                    espacio</h1>
+                <p class="flex text-[1.5em] text-[#787878]">Nuestro sistema analiza las condiciones reales <br> de tu
+                    hogar y te recomienda las plantas que <br> mejor se adaptan a ti.</p>
+                <a class="flex items-center px-1 py-5 justify-center text-[1.5em] w-[65%] bg-[#7cb22b] text-white rounded-[20px] font-bold hover:scale-110 transition duration-300"
+                    href="#">Empezar ahora</a>
                 <p class="flex text-[1.2em] text-[#787878]">Recomendaciones 100% personalizadas</p>
             </div>
             <div class="flex-1 flex items-center justify-center p-4 pr-10">
@@ -54,8 +57,304 @@
 
         </section>
 
+        <section class="flex flex-col items-center justify-center gap-2 p-20 bg-[#f6f7f6]">
+
+            <div class="flex items-center justify-center text-[#0d3225] font-bold text-[2.5em]">
+                <h2>Cómo funciona</h2>
+            </div>
+            <div class="flex items-center p-6">
+
+                <div class="flex flex-col items-center justify-center gap-4">
+
+                    <div class="flex items-center justify-center w-40 h-40 bg-[#eef0e9] rounded-full">
+
+                        <img class="w-30 h-30 object-contain" src="{{ asset('images/paso_1.png') }}"
+                            alt="imagen de un tablero verde">
+
+                    </div>
+
+                    <h3 class="flex items-center justify-center w-10 h-10 bg-[#80b42e] text-white rounded-full">1</h3>
+                    <h3 class="text-[#304a43] text-[1.5em] font-bold text-center">Cuéntanos sobre <br> tu espacio</h3>
+                    <p class="text-center text-[#696c6d]">Responde algunas preguntas sobre la luz, riesgo, espacio,
+                        experiencia y si tienes mascotas</p>
+
+                </div>
+
+                <div class="flex items-start h-50">
+
+                    <img class="w-50 h-auto" src="{{ asset('images/separacion.png') }}"
+                        alt="separacion de puntos verdes">
+
+                </div>
+
+                <div class="flex flex-col items-center justify-center gap-4">
+
+                    <div class="flex items-center justify-center w-40 h-40 bg-[#eef0e9] rounded-full">
+
+                        <img class="w-30 h-30 object-contain" src="{{ asset('images/paso_2.png') }}"
+                            alt="imagen de un tablero verde">
+
+                    </div>
+
+                    <h3 class="flex items-center justify-center w-10 h-10 bg-[#80b42e] text-white rounded-full">2</h3>
+                    <h3 class="text-[#304a43] text-[1.5em] font-bold text-center">Recibe recomendaciones <br>
+                        personalizadas</h3>
+                    <p class="text-center text-[#696c6d]">Nuestro sistema analiza la información y encuentra las plantas
+                        ideales para ti y tu entorno</p>
+
+                </div>
+
+                <div class="flex items-start h-50">
+
+                    <img class="w-50 h-auto" src="{{ asset('images/separacion.png') }}"
+                        alt="separacion de puntos verdes">
+
+                </div>
+
+                <div class="flex flex-col items-center justify-center gap-4">
+
+                    <div class="flex items-center justify-center w-40 h-40 bg-[#eef0e9] rounded-full">
+
+                        <img class="w-30 h-30 object-contain" src="{{ asset('images/paso_3.png') }}"
+                            alt="imagen de un tablero verde">
+
+                    </div>
+
+                    <h3 class="flex items-center justify-center w-10 h-10 bg-[#80b42e] text-white rounded-full">3</h3>
+                    <h3 class="text-[#304a43] text-[1.5em] font-bold text-center">Elige tu planta <br> ideal</h3>
+                    <p class="text-center text-[#696c6d]">Explora las opciones recomendadas y elige la planta perfecta
+                        para convertir tu espacio</p>
+
+                </div>
+
+            </div>
+
+        </section>
+
+        <section class="flex flex-col items-center px-3 py-20 gap-8">
+
+            <div class="flex flex-col items-center justify-center w-full">
+                <h2 class="text-[2.5em] text-[#063829] font-bold">Plantas recomendadas para ti</h2>
+                <p class="text-[1.4em] text-[#818181]">Seleccionadas según las condiciones de tu espacio</p>
+            </div>
+
+            <div class="grid grid-cols-4 px-10 gap-15 w-full">
+                <div
+                    class="overflow-hidden flex flex-col items-center justify-center rounded-[2em] border-3 border-[#ecedea]">
+
+                    <div class="flex items-center justify-center bg-[#f1f1ef] h-62.5 w-full">
+
+                        <img class="w-[80%] h-full object-contain" src="{{ asset('images/sansevieria.png') }}"
+                            alt="imagen de planta sansevieria">
+
+                    </div>
+                    <div class="flex flex-col w-full pl-6 pt-5 gap-3 border-t-3 border-[#ecedea]">
+
+                        <h3 class="text-[1.4em] tex-[#0b271e] font-semibold">Sansevieria</h3>
+                        <p class="text-[#747474]">Resistente y purificadora</p>
+
+                        <div class="flex py-4 gap-15 text-[#747474] font-semibold">
+
+                            <p>Luz baja</p>
+                            <p>Riesgo bajo</p>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div
+                    class="overflow-hidden flex flex-col items-center justify-center rounded-[2em] border-3 border-[#ecedea]">
+
+                    <div class="flex items-center justify-center bg-[#f1f1ef] h-62.5 w-full">
+
+                        <img class="w-[80%] h-full object-contain" src="{{ asset('images/photo.png') }}"
+                            alt="imagen de planta photo">
+
+                    </div>
+                    <div class="flex flex-col w-full pl-6 pt-5 gap-3 border-t-3 border-[#ecedea]">
+
+                        <h3 class="text-[1.4em] tex-[#0b271e] font-semibold">Photo</h3>
+                        <p class="text-[#747474]">Fácil de cuidar</p>
+
+                        <div class="flex py-4 gap-15 text-[#747474] font-semibold">
+
+                            <p>Luz baja</p>
+                            <p>Riesgo bajo</p>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div
+                    class="overflow-hidden flex flex-col items-center justify-center rounded-[2em] border-3 border-[#ecedea]">
+
+                    <div class="flex items-center justify-center bg-[#f1f1ef] h-62.5 w-full">
+
+                        <img class="w-[80%] h-full object-contain" src="{{ asset('images/lirio_paz.png') }}"
+                            alt="imagen de planta lirio de paz">
+
+                    </div>
+                    <div class="flex flex-col w-full pl-6 pt-5 gap-3 border-t-3 border-[#ecedea]">
+
+                        <h3 class="text-[1.4em] tex-[#0b271e] font-semibold">Lirio de paz</h3>
+                        <p class="text-[#747474]">Flor elegante y purificadora</p>
+
+                        <div class="flex py-4 gap-15 text-[#747474] font-semibold">
+
+                            <p>Luz baja</p>
+                            <p>Riesgo bajo</p>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div
+                    class="overflow-hidden flex flex-col items-center justify-center rounded-[2em] border-3 border-[#ecedea]">
+
+                    <div class="flex items-center justify-center bg-[#f1f1ef] h-62.5 w-full">
+
+                        <img class="w-[80%] h-full object-contain" src="{{ asset('images/zamioculca.png') }}"
+                            alt="imagen de planta zamioculca">
+
+                    </div>
+                    <div class="flex flex-col w-full pl-6 pt-5 gap-3 border-t-3 border-[#ecedea]">
+
+                        <h3 class="text-[1.4em] tex-[#0b271e] font-semibold">Zamioculca</h3>
+                        <p class="text-[#747474]">Ideal para principiantes</p>
+
+                        <div class="flex py-4 gap-15 text-[#747474] font-semibold">
+
+                            <p>Luz baja</p>
+                            <p>Riesgo bajo</p>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+            </div>
+
+            <div>
+
+                <a class="flex items-center justify-center py-5 px-20 border-3 border-[#cee5b8] rounded-[1.2em] font-semibold text-[#92b866] text-[1.3em] hover:scale-110 transition duration-300"
+                    href="#">Ver Catalogo completo</a>
+
+            </div>
+
+        </section>
+
+        <section class="flex items-center justify-center py-20 px-40 bg-[#f7f8f6]">
+
+            <div class="flex-1 flex flex-col items-start justify-center gap-10">
+
+                <h2 class="text-[2.5em] text-[#063829] font-bold">¿Por qué elegir Adaptia</h2>
+
+                <div class="flex items-center gap-2">
+                    <div class="flex items-center justify-center p-3 bg-[#7aae2a] rounded-[50%]">
+
+                        <img class="size-[2em]" src="{{ asset('images/hoja.png') }}" alt="imagen de una plantita">
+
+                    </div>
+                    <div>
+                        <p class="text-[#315b4a] font-semibold text-[1.2em]">Recomendaciones precisas</p>
+                        <p class="text-[#737f7d]">Basadas en datos reales de tu entorno</p>
+                    </div>
+                </div>
+
+                <div class="flex items-center gap-2">
+                    <div class="flex items-center justify-center p-3 bg-[#7aae2a] rounded-[50%]">
+
+                        <img class="size-[2em]" src="{{ asset('images/reloj.png') }}" alt="imagen de una reloj">
+
+                    </div>
+                    <div>
+                        <p class="text-[#315b4a] font-semibold text-[1.2em]">Recomendaciones precisas</p>
+                        <p class="text-[#737f7d]">Basadas en datos reales de tu entorno</p>
+                    </div>
+                </div>
+
+                <div class="flex items-center gap-2">
+                    <div class="flex items-center justify-center p-3 bg-[#7aae2a] rounded-[50%]">
+
+                        <img class="size-[2em]" src="{{ asset('images/corazon.png') }}" alt="imagen de un corazon">
+
+                    </div>
+                    <div>
+                        <p class="text-[#315b4a] font-semibold text-[1.2em]">Recomendaciones precisas</p>
+                        <p class="text-[#737f7d]">Basadas en datos reales de tu entorno</p>
+                    </div>
+                </div>
+
+
+
+            </div>
+            <div class="flex-1 flex items-center justify-center">
+
+                <img class="w-[60%] h-auto" src="{{ asset('images/logo.png') }}" alt="imagen del logo">
+
+            </div>
+
+        </section>
+
     </main>
-    <footer>
+    <footer class="flex flex-col items-center p-6 px-50 justify-center text-white bg-[#053a28] gap-10 w-full">
+
+        <div class="flex gap-20 w-full">
+
+            <div class="flex-1 flex items-center justify-center text-left">
+
+                {{-- imagen --}}
+                <p>Tecnología y naturaleza para ayudarte a vivir en armonía con las plantas</p>
+
+            </div>
+            <div class="flex-2 flex justify-between">
+
+                <div class="flex flex-col gap-6">
+
+                    <h3 class="font-bold">Navegación</h3>
+                    <ul class="flex flex-col gap-3">
+                        <li>Cómo funciona</li>
+                        <li>Catálogo</li>
+                        <li>Nosotros</li>
+                    </ul>
+
+                </div>
+                <div class="flex flex-col gap-6">
+
+                    <h3 class="font-bold">Recursos</h3>
+                    <ul class="flex flex-col gap-3">
+                        <li>Guía de cuidados</li>
+                        <li>Preguntas frecuentes</li>
+                        <li>Blog</li>
+                    </ul>
+
+                </div>
+                <div class="flex flex-col gap-6">
+
+                    <h3 class="font-bold">Legal</h3>
+                    <ul class="flex flex-col gap-3">
+                        <li>Términos y condiciones</li>
+                        <li>Política de privacidad</li>
+                        <li>Cookies</li>
+                    </ul>
+
+                </div>
+
+            </div>
+
+        </div>
+
+
+
+        <p>© 2026 Adaptia. Todos los derechos reservados</p>
 
     </footer>
 </body>
