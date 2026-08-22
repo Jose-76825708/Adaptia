c0fd9263a0751a83bf2139ea909e3397ec9a68a1
Jose-76825708 <76825708@continental.edu.pe>
2026-08-22 18:35:23 -0500

    finalización de la creacion del home completado de iconos y animaciones

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,27 @@
 import './bootstrap';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const reveals = document.querySelectorAll('.reveal');
+
+    const observer = new IntersectionObserver((entries) => {
+
+        entries.forEach(entry => {
+
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target);
+            }
+
+        });
+
+    }, {
+        threshold: 0.15
+    });
+
+    reveals.forEach(element => {
+        observer.observe(element);
+    });
+
+});
