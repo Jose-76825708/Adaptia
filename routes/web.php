6695112667dcd45db06bac2ae7a7e1256aa6b9a2
Jose-76825708 <76825708@continental.edu.pe>
2026-08-29 15:59:58 -0500

    Guarado de cambio de route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
-   use App\Http\Controllers\PlantaController;
+use App\Http\Controllers\PlantaController;
 use App\Http\Controllers\TipoPlantaController;
 
 Route::get('/home', [HomeController::class, 'index']);
